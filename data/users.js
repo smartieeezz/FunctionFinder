58210ec82e938453f515f76b233d71a91988bcff
TannerMarshall551 <tmarsha1@stevens.edu>
2023-04-27 19:34:11 -0400

    Added login push route

--- data/users.js
+++ data/users.js
@@ -47,25 +47,25 @@ const exportedMethods = {
         }
 
         //check if we have a valid email address and if we do then convert to lowercase and trim it
-        email = checkEmail(email)
+        email = checkEmail(email);
         //If the firstName, lastName or username are empty strings we will throw an error
         firstName = firstName.trim();
         lastName = lastName.trim();
         username = username.trim();
-        dateOfBirth = dateOfBirth.trim()
+        dateOfBirth = dateOfBirth.trim();
 
-        dateOfBirth = checkAge(dateOfBirth)
+        dateOfBirth = checkAge(dateOfBirth);
         //connect to our database for users
         const usersCollection = await users();
         const existingEmail = await usersCollection.findOne({ email: email.toLowerCase() });
         //if we have that email address in our database already then we will throw an error
         if (existingEmail) {
-            throw 'Error: A user with that email address already exists.' 
+            throw 'Error: A user with that email address already exists.'; 
         }
         const existingUserName = await usersCollection.findOne({ username: username.toLowerCase() });
         //if we have that username in our database already then we will throw an error
         if (existingUserName) {
-            throw 'Error: A user with that username already exists.' 
+            throw 'Error: A user with that username already exists.'; 
         }
 
         if (firstName == "") {
@@ -77,7 +77,7 @@ const exportedMethods = {
         if (username == "") {
         throw `Error: The name cannot consist of just empty space`;
         }
-        dateOfBirth = checkAge(dateOfBirth)
+        dateOfBirth = checkAge(dateOfBirth);
         
         //use bcrypt to hash the passwords
 
@@ -116,50 +116,50 @@ const exportedMethods = {
     async checkUser (emailAddress, password) {
         //Both emailAddress and password must be supplied or you will throw an error
         if (!emailAddress) {
-            throw `Error: The emailAddress must be provided.`
+            throw `Error: The Email Address must be provided.`;
         }
         if (!password) {
-            throw `Error: The password must be provided.`
+            throw `Error: The password must be provided.`;
         }
         //emailAddress should be a valid email address format. example@example.com
-        emailAddress = checkEmail(emailAddress)
+        emailAddress = checkEmail(emailAddress);
         
 
         /*Query the db for the emailAddress supplied, if it is not found, throw an error stating 
         "Either the email address or password is invalid".*/
-        const usersCollections = await users()
+        const usersCollections = await users();
         const existingUser = await usersCollections.findOne({ email: emailAddress.toLowerCase()});
         if (!existingUser) {
-            throw 'Error: A user with that email address does not exist.' 
+            throw 'Error: A user with that email address does not exist.'; 
         }
         /*If the emailAddress supplied is found in the DB, you will then use bcrypt to compare the
         hashed password in the database with the password input parameter.*/
         const passwordsMatch = await bcrypt.compare(password, existingUser.password);
         if (!passwordsMatch) {
-            throw `Either the email address or password is invalid.`
+            throw `Invalid password.`;
         }
-        return `Welcome ${existingUser.firstName} ${existingUser.lastName}.`
+        return existingUser;
 
     },
 
     async get (id) {
         //check if the id exists
         if (!id) {
-            throw `Error: The ID must be provided. `
+            throw `Error: The ID must be provided. `;
         }
         //check if the id is a string
         if (typeof id!=='string') {
-            throw `Error: The ID must be a string.`
+            throw `Error: The ID must be a string.`;
         }
         //trim the id to get rid of whitespace at the edges of the string
-        id = id.trim()
+        id = id.trim();
         if (id.length==0 || id=="") {
-            throw `Error: The ID must not be empty spaces.`
+            throw `Error: The ID must not be empty spaces.`;
         }
     
         //if we hve no errors from the ID we can just load the bands database now
         if (!ObjectId.isValid(id)) {
-            throw 'invalid object ID'
+            throw 'invalid object ID';
         }
         const usersCollection = await users();
         const thisUser = await usersCollection.findOne({_id: new ObjectId(id)});
@@ -173,14 +173,14 @@ const exportedMethods = {
 
     async getByEmail(email) {
         if (!email) {
-            throw `Error: The email was not given.`
+            throw `Error: The email was not given.`;
         }
-        email = email.trim()
-        const usersCollection = await users()
-        const thisUser = await usersCollection.find({email})
+        email = email.trim();
+        const usersCollection = await users();
+        const thisUser = await usersCollection.find({email});
 
         if (typeof thisUser!=='undefined' || thisUser!==null) {
-            throw `Error: The email was used already.`
+            throw `Error: The email was used already.`;
         }
     },
 
