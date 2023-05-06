ec0a9ae07fee790580e0343c955697e2f881c3d1
TannerMarshall551 <tmarsha1@stevens.edu>
2023-05-06 19:43:47 -0400

    Fixed the signout button and added middleware to ensure user is signed in when searching for party.

--- app.js
+++ app.js
@@ -105,6 +105,14 @@ app.use('/account/create', (req, res, next) => {
   next();
 });
 
+app.use('/searchresults', (req, res, next) => {
+  if(!req.session.user){
+    loggedIn = false;
+    return res.redirect('/account/login');
+  }
+  next();  
+});
+
 app.use('/', (req, res, next) => {
   if (req.session.user)
     loggedIn = true;
