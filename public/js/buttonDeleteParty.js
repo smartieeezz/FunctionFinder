0461d6872fc0a10366880d34eb2d65f5e346eb5a
rjohn92 <rolandjohn92@gmail.com>
2023-05-09 00:00:13 -0400

    removed delete button

--- public/js/buttonDeleteParty.js
+++ public/js/buttonDeleteParty.js
@@ -1,23 +1,23 @@
-unregisterButton.addEventListener("click", function() {
-    const eventId = this.getAttribute("delete-event-id");
-    const userId = this.getAttribute("delete-user-id");
-    const action = this.getAttribute("delete-action");
+// unregisterButton.addEventListener("click", function() {
+//     const eventId = this.getAttribute("delete-event-id");
+//     const userId = this.getAttribute("delete-user-id");
+//     const action = this.getAttribute("delete-action");
 
-    if (!userId) {
-        window.location.href = "/account/login";
-        return;
-      }
+//     if (!userId) {
+//         window.location.href = "/account/login";
+//         return;
+//       }
     
-      if (action !== "delete") {
-        return;
-      }
-      fetch(`/events/${eventId}`)
-    .then(response => response.json())
-    .then(event => {
-      if (event.partyHost === userId) {
-        alert("This will delete the event for everyone.");
-        return;
-      }
-})
+//       if (action !== "delete") {
+//         return;
+//       }
+//       fetch(`/events/${eventId}`)
+//     .then(response => response.json())
+//     .then(event => {
+//       if (event.partyHost === userId) {
+//         alert("This will delete the event for everyone.");
+//         return;
+//       }
+// })
 
-})
+// })
