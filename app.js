8ba9e3d33780c62b29c708e10de5e6efe81586da
TannerMarshall551 <tmarsha1@stevens.edu>
2023-05-09 18:48:10 -0400

    Added share buttons for twitter and gmail

--- app.js
+++ app.js
@@ -127,13 +127,13 @@ app.use('/account/create', (req, res, next) => {
   next();
 });
 
-app.use('/searchresults', (req, res, next) => {
-  if(!req.session.user){
-    loggedIn = false;
-    return res.redirect('/account/login');
-  }
-  next();  
-});
+// app.use('/searchresults', (req, res, next) => {
+//   if(!req.session.user){
+//     loggedIn = false;
+//     return res.redirect('/account/login');
+//   }
+//   next();  
+// });
 
 app.use('/events/:id/info', (req, res, next) => {
   if(!req.session.user){
@@ -150,6 +150,8 @@ app.use('/events/:id/comments', (req, res, next) => {
   }
   next();  
 });
+
+
 
 app.use('/', (req, res, next) => {
   if (req.session.user)
